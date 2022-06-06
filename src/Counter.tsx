1b33013d36cd4aa23b3e45057b2740e03f525bb0
CJ Guttormsson <cj.guttormsson@gmail.com>
2022-06-06 14:18:25 -0600

    Update Counter and TempConv to use Bootstrap

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,18 +4,22 @@ const Counter = (props: any) => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>The count is {count}</p>
-      <p>
-        <button
-          onClick={() => {
-            setCount((c) => c + 1);
-          }}
-        >
-          Increment
-        </button>
-      </p>
-    </>
+    <div className="row justify-content-center">
+      <div className="col-3">
+        <div className="input-group m2">
+          <input
+            aria-label="Current Count"
+            className="form-control"
+            type="text"
+            value={count}
+            disabled
+          />
+          <button className="btn btn-primary" type="button" onClick={() => setCount((c) => c + 1)}>
+            Count
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/TemperatureConverter.tsx
+++ src/TemperatureConverter.tsx
@@ -5,28 +5,25 @@ const cToF = (c: number) => c * (9 / 5) + 32;
 const fToC = (f: number) => ((f - 32.0) * 5) / 9;
 
 /**
- * A utility to convert between Celcius and Fahrenheit in two text boxes. The values are truncated
+ * A utility to convert between Celsius and Fahrenheit in two text boxes. The values are truncated
  * to one decimal point.
  *
  * @param props unused
  */
 const TemperatureConverter = (props: any) => {
-  const [celcius, setCelcius] = useState("-40.0");
+  const [celsius, setCelsius] = useState("-40.0");
   const [fahrenheit, setFahrenheit] = useState("-40.0");
 
-  const trySetCelcius = useCallback(
-    (newCelcius: string, valueTransform = (x: number) => x) => {
-      const maybeCelcius = Number.parseFloat(newCelcius);
-      if (!Number.isNaN(maybeCelcius)) {
-        const fixedCelcius = valueTransform(maybeCelcius).toFixed(1);
-        setCelcius(fixedCelcius);
-        return fixedCelcius;
-      } else {
-        return "";
-      }
-    },
-    []
-  );
+  const trySetCelsius = useCallback((newCelsius: string, valueTransform = (x: number) => x) => {
+    const maybeCelsius = Number.parseFloat(newCelsius);
+    if (!Number.isNaN(maybeCelsius)) {
+      const fixedCelsius = valueTransform(maybeCelsius).toFixed(1);
+      setCelsius(fixedCelsius);
+      return fixedCelsius;
+    } else {
+      return "";
+    }
+  }, []);
 
   const trySetFahrenheit = useCallback(
     (newFahrenheit: string, valueTransform = (x: number) => x) => {
@@ -43,36 +40,47 @@ const TemperatureConverter = (props: any) => {
   );
 
   return (
-    <p>
-      <input
-        key="inputC"
-        type="text"
-        value={celcius}
-        onChange={(e) => {
-          setCelcius(e.target.value);
-          trySetFahrenheit(e.target.value, cToF);
-        }}
-        onBlur={() => {
-          // Round to 1 decimal on blur, then update the other value if necessary
-          trySetFahrenheit(trySetCelcius(celcius), cToF);
-        }}
-      ></input>
-      {" Celcius = "}
-      <input
-        key="inputF"
-        type="text"
-        value={fahrenheit}
-        onChange={(e) => {
-          setFahrenheit(e.target.value);
-          trySetCelcius(e.target.value, fToC);
-        }}
-        onBlur={() => {
-          // Round to 1 decimal on blur, then update the other value if necessary
-          trySetCelcius(trySetFahrenheit(fahrenheit), fToC);
-        }}
-      ></input>
-      {" Fahrenheit"}
-    </p>
+    <div className="row justify-content-center">
+      <div className="col-6">
+        <div className="input-group m2">
+          <input
+            aria-describedby="label-celsius"
+            aria-label="Celsius"
+            className="form-control"
+            type="text"
+            value={celsius}
+            onChange={(e) => {
+              setCelsius(e.target.value);
+              trySetFahrenheit(e.target.value, cToF);
+            }}
+            onBlur={() => {
+              trySetFahrenheit(trySetCelsius(celsius), cToF);
+            }}
+          />
+          <span id="label-celsius" className="input-group-text">
+            ° Celsius
+          </span>
+          <span className="input-group-text">=</span>
+          <input
+            aria-describedby="label-fahrenheit"
+            aria-label="Fahrenheit"
+            className="form-control"
+            type="text"
+            value={fahrenheit}
+            onChange={(e) => {
+              setFahrenheit(e.target.value);
+              trySetCelsius(e.target.value, fToC);
+            }}
+            onBlur={() => {
+              trySetCelsius(trySetFahrenheit(fahrenheit), fToC);
+            }}
+          />
+          <span id="label-fahrenheit" className="input-group-text">
+            ° Fahrenheit
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
