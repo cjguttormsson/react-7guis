f54c6e7e9e9a26b8cbbf14e7563f681cfafb2572
CJ Guttormsson <cjguttormsson@users.noreply.github.com>
2022-06-12 18:30:14 -0700

    Create custom useHistory hook for undo/redo

---
+++ src/useHistory.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function useHistory<T>(initialVal: T): [presentValue: T, setPresentValue: (newVal: T) => void, undo?: () => void, redo?: () => void] {
+  const [past, setPast] = useState([] as T[]);
+  const [presentValue, setPresentValue] = useState(initialVal);
+  const [future, setFuture] = useState([] as T[]);
+
+  const setValue = (newValue: T) => {
+    setPast([...past, presentValue]);
+    setPresentValue(newValue);
+    setFuture([]);
+  };
+
+  const undo = (past.length > 0) ? () => {
+    setFuture([presentValue, ...future]);
+    setPresentValue(past.at(-1)!);
+    setPast(past.slice(0, -1));
+  } : undefined;
+
+  const redo = (future.length > 0) ? () => {
+    setPast([...past, presentValue]);
+    setPresentValue(future.at(0)!);
+    setFuture(future.slice(1));
+  } : undefined;
+
+  console.log(past, presentValue, future);
+  return [presentValue, setValue, undo, redo];
+}
+
+export default useHistory;
